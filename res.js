6eacf0fd5dae42d53fc9091a53134c945951e6e6
Juangranda3424 <juan.granda3424@gmail.com>
2026-01-27 12:34:26 -0500

    add function res with its test

---
+++ res.js
@@ -0,0 +1,5 @@
+const res = (a,b)=>{
+    return a-b;
+};
+
+module.exports = res;
---
+++ res.test.js
@@ -0,0 +1,7 @@
+const res = require('./res');
+
+test('Resta de 4 - 3 = 1', () => {
+    expect(res(4,3)).toBe(1);
+});
+
+
